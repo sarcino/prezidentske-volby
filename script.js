17e7edf8dcb1888a64e57338c493624aba64a807
sarcino <sarka.sevci@gmail.com>
2018-01-22 22:04:08 +0100

    js, divs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 
+
     var callAPI = function () {
         $.ajax({
             type: "get",
@@ -7,8 +8,25 @@ window.onload = function () {
             contentType: "application/xml",
             url: "http://api.jsemsarka.cz",
             success: function (xml) {
-
                 console.log(xml);
+                var allCandidates = xml.getElementsByTagName("KANDIDAT");
+
+                var allNames = [];
+                var values = [];
+
+                // PROCHÁZÍM VŠECHNY KANDIDÁTY A VYBÍRÁM JMÉNO, PŘÍJMENÍ A PROCENTA HLASŮ
+                for (var i = 0; i < allCandidates.length; i++) {
+                    allNames.push(allCandidates[i].getAttribute("JMENO") + " " + allCandidates[i].getAttribute("PRIJMENI"));
+                    values.push(allCandidates[i].getAttribute("HLASY_PROC_1KOLO"));
+                }
+                document.getElementById("results2").innerHTML = allNames;
+                document.getElementById("results3").innerHTML = values;
+
+                // GRAF
+
+              
+                
+// END OF SUCCESS
             },
 
         });

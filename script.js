988895fe7005e6483c479477b86150bf7359a5dc
sarcino <sarka.sevci@gmail.com>
2018-01-23 22:40:43 +0100

    jquery

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 window.onload = function () {
+    
+    // TABS
+    //SLIDE DOWN = SHOW
+    //SLIDE UP = HIDE
+    $('div.first').slideUp();
+    $('#second').click(function() {
+        $('div.first').slideUp();
+		// Zobraz DIV s class .first
+		$('div.second').slideDown();
+        
+    });
+                       
+    $('#first').click(function() {
+		$('div.second').slideUp();
+		$('div.first').slideDown();
+        
+	});
+    
+    $('h2').click(function() {
+        $('h2').removeClass('current-tab');
+        $(this).addClass('current-tab');
+    });
+                      
 
     var callAPI = function () {
         $.ajax({
             type: "get",
             dataType: "xml",
             contentType: "application/xml",
-           // url: "http://api.jsemsarka.cz",
+           // url: "https://api.jsemsarka.cz",
             
-            url: "http://api.jsemsarka.cz/?offline=1",
+            url: "https://api.jsemsarka.cz/?offline=1",
             success: function (xml) {
 
 
@@ -15,26 +38,25 @@ window.onload = function () {
 
                 var allNames = [];
                 var values = [];
-                var values2 = [];
-                var allNames2 = [];
+                var valuesTwo = [];
+                var allNamesTwo = [];
 
                 // PROCHÁZÍM VŠECHNY KANDIDÁTY A VYBÍRÁM JMÉNO, PŘÍJMENÍ A PROCENTA HLASŮ
                 for (var i = 0; i < allCandidates.length; i++) {
                     allNames.push(allCandidates[i].getAttribute("JMENO") + " " + allCandidates[i].getAttribute("PRIJMENI"));
                     values.push(allCandidates[i].getAttribute("HLASY_PROC_1KOLO"));
 
-                    if (allCandidates[i].getAttribute("PORADOVE_CISLO") === 7 || allCandidates[i].getAttribute("PORADOVE_CISLO") === 9) {
-                        allNames2.push(allCandidates[i].getAttribute("JMENO") + " " + allCandidates[i].getAttribute("PRIJMENI"));
-                        values2.push(allCandidates[i].getAttribute("HLASY_PROC_2KOLO"));
+                    if (allCandidates[i].getAttribute("PORADOVE_CISLO") == 7 || allCandidates[i].getAttribute("PORADOVE_CISLO") == 9) {
+                        allNamesTwo.push(allCandidates[i].getAttribute("JMENO") + " " + allCandidates[i].getAttribute("PRIJMENI"));
+                        valuesTwo.push(allCandidates[i].getAttribute("HLASY_PROC_2KOLO"));
 
                     }
 
                 }
                 // document.getElementById("results").innerHTML = allNames;
                 // document.getElementById("results1").innerHTML = values;
-                
-                 document.getElementById("results2").innerHTML = allNames2;
-                 document.getElementById("results3").innerHTML = values2;
+                 document.getElementById("results2").innerHTML = allNamesTwo;
+                 document.getElementById("results3").innerHTML = valuesTwo;
 
                 // GRAPH - 1. KOLO
 
